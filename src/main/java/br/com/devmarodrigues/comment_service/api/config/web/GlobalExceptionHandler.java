5dfc6085b0a512d130151c1fa48815cd3213938f
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2026-01-02 07:57:53 -0300

    Integration by RestClient

--- src/main/java/br/com/devmarodrigues/comment_service/api/config/web/GlobalExceptionHandler.java
+++ src/main/java/br/com/devmarodrigues/comment_service/api/config/web/GlobalExceptionHandler.java
@@ -1,12 +1,20 @@
 package br.com.devmarodrigues.comment_service.api.config.web;
 
+import br.com.devmarodrigues.comment_service.api.config.web.customExceptions.ModerateServiceClientBadGatewayException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.validation.FieldError;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.net.URI;
+import java.nio.channels.ClosedChannelException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,5 +30,26 @@ public class GlobalExceptionHandler {
             errors.put(fieldName, errorMessage);
         });
         return new ResponseEntity<>(errors, HttpStatus.UNPROCESSABLE_CONTENT);
+    }
+
+    @ExceptionHandler(ModerateServiceClientBadGatewayException.class)
+    public ResponseEntity<Object> handleValidationExceptions(ModerateServiceClientBadGatewayException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("Forbiddend words: ", ex.getMessage());
+        return new ResponseEntity<>(errors, HttpStatus.UNPROCESSABLE_CONTENT);
+    }
+
+    @ExceptionHandler({
+        SocketTimeoutException.class,
+        ConnectException.class,
+        ClosedChannelException.class
+    })
+    public ProblemDetail handle(IOException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.GATEWAY_TIMEOUT);
+        problemDetail.setTitle("Gateway timeou");
+        problemDetail.setDetail(e.getMessage());
+        problemDetail.setType(URI.create("/errors/gateway-timeout"));
+
+        return problemDetail;
     }
 }
--- src/main/java/br/com/devmarodrigues/comment_service/api/controller/CommentController.java
+++ src/main/java/br/com/devmarodrigues/comment_service/api/controller/CommentController.java
@@ -1,17 +1,24 @@
 package br.com.devmarodrigues.comment_service.api.controller;
 
+import br.com.devmarodrigues.comment_service.api.client.ModerateServiceClient;
+import br.com.devmarodrigues.comment_service.api.config.web.customExceptions.ModerateServiceClientBadGatewayException;
 import br.com.devmarodrigues.comment_service.api.model.CommentInput;
 import br.com.devmarodrigues.comment_service.api.model.CommentOutput;
+import br.com.devmarodrigues.comment_service.api.model.ModerationOutput;
+import br.com.devmarodrigues.comment_service.api.model.ModerationResult;
 import br.com.devmarodrigues.comment_service.domain.model.Comment;
 import br.com.devmarodrigues.comment_service.domain.model.repositoty.CommentRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -23,7 +30,10 @@ import java.util.UUID;
 @RequestMapping("/api/comments")
 public class CommentController {
 
-    private CommentRepository commentRepository;
+    private static Logger logger = LogManager.getLogger(CommentController.class);
+
+    private final CommentRepository commentRepository;
+    private final ModerateServiceClient commentServiceClient;
 
     @GetMapping
     public Page<CommentOutput> getAll(@PageableDefault Pageable pageable) {
@@ -40,17 +50,33 @@ public class CommentController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CommentOutput create(@RequestBody @Valid CommentInput commentInput) {
-        Comment comment = Comment.builder()
-            .id(UUID.randomUUID())
+    public ResponseEntity<CommentOutput> create(@RequestBody @Valid CommentInput commentInput) throws ModerateServiceClientBadGatewayException {
+
+        UUID commentId = UUID.randomUUID();
+
+        ModerationResult result = commentServiceClient.moderate(ModerationOutput
+            .builder()
+            .commentId(commentId)
             .text(commentInput.getText())
-            .author(commentInput.getAuthor())
-            .createdAt(LocalDateTime.now())
-            .build();
+            .build());
 
-        comment = commentRepository.saveAndFlush(comment);
+        logger.info(String.format("Result recived: %s", result.getApproved()));
 
-        return convertToModel(comment);
+        if (result.getApproved()) {
+
+            Comment comment = Comment.builder()
+                    .id(commentId)
+                    .text(commentInput.getText())
+                    .author(commentInput.getAuthor())
+                    .createdAt(LocalDateTime.now())
+                    .build();
+
+            comment = commentRepository.saveAndFlush(comment);
+
+            return new ResponseEntity<>(convertToModel(comment), HttpStatus.CREATED);
+        }
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).build();
     }
 
     private CommentOutput convertToModel(@NonNull Comment comment) {
@@ -61,5 +87,4 @@ public class CommentController {
             .createdAt(comment.getCreatedAt())
             .build();
     }
-
 }
